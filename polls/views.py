4fc5da3ecee9e191b0093bc6c99405a48ca6e1b4
Anton Makridin <a.i.makridin@yandex.ru>
2018-02-20 14:59:38 +0300

    p4 more templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
 from django.template import loader
 from .models import Question
 
@@ -27,9 +27,25 @@ def detail(request, question_id):
 		raise Http404('Not_found')
 	return render(request, 'polls/detail.html', {'question': question})
 
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{
+			'question': question,
+			'error_message': 'no_q_found',
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
 
 def results(request, question_id):
-	return HttpResponse("You're looking at results of %s" % question_id)
+	# return HttpResponse("You're looking at results of %s" % question_id
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/result.html', {'question':question})
 
 def vote(request, question_id):
 	return HttpResponse("You're looking vote of %s" % question_id)
